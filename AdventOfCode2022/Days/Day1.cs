6fd1d0a9c389f3f5e62f841f66887c2411b5e557
Kevin <kvnjager@gmail.com>
2023-08-26 06:52:41 +0200

    Day 1

--- AdventOfCode2022/Days/Day1.cs
+++ AdventOfCode2022/Days/Day1.cs
@@ -1,4 +1,5 @@
 ﻿using AdventOfCode2021.Interface;
+using System.Collections;
 
 namespace AdventOfCode2021.Days
 {
@@ -6,12 +7,46 @@ namespace AdventOfCode2021.Days
     {
         public string SolvePart1()
         {
-            throw new NotImplementedException();
+            int highest = 0;
+            string[] input = Utils.LoadFileToStringArray("Day1-1.txt");
+
+            int currentCounter = 0;
+            
+            foreach (string item in input)
+            {
+                if (string.IsNullOrEmpty(item))
+                {
+                    if (currentCounter > highest)
+                    {
+                        highest = currentCounter;
+                    }
+                    currentCounter = 0;
+                    continue;
+                }
+                currentCounter += int.Parse(item);
+            }
+
+            return highest.ToString();
         }
 
         public string SolvePart2()
         {
-            throw new NotImplementedException();
+            List<int> calories = new List<int>();
+            string[] input = Utils.LoadFileToStringArray("Day1-1.txt");
+
+            int currentCounter = 0;
+            foreach (string item in input)
+            {
+                if (string.IsNullOrEmpty(item))
+                {
+                    calories.Add(currentCounter);
+                    currentCounter = 0;
+                    continue;
+                }
+                currentCounter += int.Parse(item);
+            }
+
+            return calories.OrderByDescending(i => i).Take(3).Sum().ToString();
         }
     }
 }
